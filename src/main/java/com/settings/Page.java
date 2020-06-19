3306f0b2ae099ea2d57ba6c9ced2bc55651b939b
Sonne <rikxela@gmail.com>
2020-06-20 01:09:27 +0300

    finalLine

--- src/main/java/com/settings/Page.java
+++ src/main/java/com/settings/Page.java
@@ -6,12 +6,22 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class Page {
+    private final String width;
+    private final String height;
+
+    public String getWidth() {
+        return width;
+    }
+
+    public String getHeight() {
+        return height;
+    }
 
     public Page(Document doc) {
         NodeList page = doc.getElementsByTagName("page");
         Node node = page.item(0);
         Element element = (Element) node;
-        String width = element.getElementsByTagName("width").item(0).getTextContent();
-        String height = element.getElementsByTagName("height").item(0).getTextContent();
+        width = element.getElementsByTagName("width").item(0).getTextContent();
+        height = element.getElementsByTagName("height").item(0).getTextContent();
     }
 }
